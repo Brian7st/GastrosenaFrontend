71490aa069f78b23e152588705fd6fa05aa27da1
JohanGomezRico <johangrico10@gmail.com>
2026-05-16 16:21:18 -0500

    feat(restaurante): agregar notas de pedido, scroll en carrito y arreglar visibilidad de boton mas

--- libs/restaurante/restaurante/src/lib/components/pedidos-cart/pedidos-cart.component.ts
+++ libs/restaurante/restaurante/src/lib/components/pedidos-cart/pedidos-cart.component.ts
@@ -1,20 +1,24 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { RestauranteFacade } from '../../data-access/restaurante.facade';
 import { ButtonComponent, LucideIconComponent } from '@restaurant/shared/ui';
 
 @Component({
   selector: 'lib-pedidos-cart',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, LucideIconComponent],
+  imports: [CommonModule, FormsModule, ButtonComponent, LucideIconComponent],
   templateUrl: './pedidos-cart.component.html',
   styleUrls: ['./pedidos-cart.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PedidosCartComponent {
   private facade = inject(RestauranteFacade);
+  private router = inject(Router);
 
   pedidoActivo = this.facade.pedidoActivo;
+  observacionesGenerales = signal('');
 
   incrementar(index: number) {
     this.facade.actualizarCantidadProducto(index, 1);
@@ -26,6 +30,7 @@ export class PedidosCartComponent {
 
   confirmarPedido() {
     this.facade.confirmarPedidoActivo();
+    this.router.navigate(['/restaurante/mesas']);
   }
 }
 
