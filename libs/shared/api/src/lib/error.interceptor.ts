d258a15df7b3d7ade06d4d48ecd20408718cd23d
Luisa23121 <luisamaria23121@gmail.com>
2026-05-29 17:36:10 -0500

    fix(auth): Correcion de direccionamiento de contadora y fallback de admin

--- libs/shared/api/src/lib/error.interceptor.ts
+++ libs/shared/api/src/lib/error.interceptor.ts
@@ -1,10 +1,24 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@restaurant/shared/auth';
 import { catchError, throwError } from 'rxjs';
 
-export const errorInterceptor: HttpInterceptorFn = (req, next) =>
-  next(req).pipe(
-    catchError(error => {
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((error) => {
       console.error('HTTP error captured by shared/api:', error);
+      
+      // Si es 401 y la petición NO es de login (para evitar bucle)
+      if (error.status === 401 && !req.url.includes('/auth/login')) {
+        authService.logout();
+        router.navigate(['/auth/login']);
+      }
+      
       return throwError(() => error);
-    }),
+    })
   );
+};
