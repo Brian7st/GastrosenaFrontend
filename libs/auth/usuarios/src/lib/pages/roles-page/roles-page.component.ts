e1e22bcf7205df28bffe726a975143754f0aeb6c
Luisa23121 <luisamaria23121@gmail.com>
2026-06-01 23:26:09 -0500

    fix(usuarios): conexion a backend y correcion de vista

--- libs/auth/usuarios/src/lib/pages/roles-page/roles-page.component.ts
+++ libs/auth/usuarios/src/lib/pages/roles-page/roles-page.component.ts
@@ -62,7 +62,7 @@ const ROLES_SIMULACION_SET = new Set<string>(ROLES_SIMULACION_INFO.map(r => r.ro
 
 const ROLES_STAFF = new Set<string>([
   Rol.ADMINISTRADOR, Rol.CONTADORA, Rol.INSTRUCTOR,
-  Rol.APRENDIZ,
+  
 ]);
 
 @Component({
