8c6a0bfea656f41a8d923575e4d159b97f2f40df
JohanGomezRico <johangrico10@gmail.com>
2026-06-03 17:10:58 -0500

    fix(restaurante): fix caja cash icon and remove propina payload

--- libs/restaurante/restaurante/src/lib/models/restaurante.model.ts
+++ libs/restaurante/restaurante/src/lib/models/restaurante.model.ts
@@ -68,7 +68,6 @@ export interface FacturaResponse {
   sesionCajaId: string;
   cajeroId: string;
   subtotal: number;
-  propina: number;
   total: number;
   metodoPago: MetodoPago;
   estado: EstadoFacturaRestaurante;
@@ -87,7 +86,6 @@ export interface CerrarSesionRequest {
 export interface FacturarPedidoRequest {
   pedidoId: string;
   metodoPago: MetodoPago;
-  propina: number;
 }
 
 /**
