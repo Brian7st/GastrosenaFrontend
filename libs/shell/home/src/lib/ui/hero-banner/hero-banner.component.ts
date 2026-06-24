2998c7045e878c2102ba53bce31808b2817b76f1
Brian7st <brianstivenacunavillarraga@gmail.com>
2026-06-23 19:56:46 -0500

    fix(shell): remover import no usado y ordenar declaraciones Sass

--- libs/shell/home/src/lib/ui/hero-banner/hero-banner.component.ts
+++ libs/shell/home/src/lib/ui/hero-banner/hero-banner.component.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'restaurant-hero-banner',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './hero-banner.component.html',
   styleUrl: './hero-banner.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
