7efd4a22c652a624a78d3f65f402e10f4733e597
Brian7st <brianstivenacunavillarraga@gmail.com>
2026-06-23 19:56:42 -0500

    fix(restaurante): remover import no usado (NG8113)

--- libs/restaurante/restaurante/src/lib/components/pedidos-menu-grid/pedidos-menu-grid.component.ts
+++ libs/restaurante/restaurante/src/lib/components/pedidos-menu-grid/pedidos-menu-grid.component.ts
@@ -4,13 +4,13 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { RestauranteFacade } from '../../data-access/restaurante.facade';
 import { ProductoMenu } from '../../data-access/restaurante.facade';
 
-import { CardComponent, StatusBadgeComponent, ConfirmDialogComponent } from '@restaurant/shared/ui';
+import { CardComponent, ConfirmDialogComponent } from '@restaurant/shared/ui';
 import { CurrencyCopPipe } from '@restaurant/shared/util';
 
 @Component({
   selector: 'lib-pedidos-menu-grid',
   standalone: true,
-  imports: [CommonModule, CardComponent, StatusBadgeComponent, ConfirmDialogComponent, CurrencyCopPipe],
+  imports: [CommonModule, CardComponent, ConfirmDialogComponent, CurrencyCopPipe],
   templateUrl: './pedidos-menu-grid.component.html',
   styleUrls: ['./pedidos-menu-grid.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
